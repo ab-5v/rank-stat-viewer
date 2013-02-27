bb77cb3ef1652eeb41c9db8795c086a68a5a2a88
Artur Burtsev <artjock@gmail.com>
2013-02-27 21:09:48 +0400

    request

--- js/index.js
+++ js/index.js
@@ -6,4 +6,13 @@
 
     $('body').html( rsv.yate({}, 'body') );
 
+    $('body')
+        .on('keypress', '.js-request-input', function(e) {
+            if (e.which == 13 && $(this).val()) {
+                $.getJSON($(this).val(), function(data) {
+                    console.log(data);
+                });
+            }
+        })
+
 })(this);
